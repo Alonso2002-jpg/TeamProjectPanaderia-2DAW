4501a8f01dec47b0365ca711f6ff07d8c7c58e07
Joselyn Obando <obandofernandez1@gmail.com>
2023-11-08 17:43:44 +0100

    Agregando en el pack dto la clase ClienteResponseDto

--- src/main/java/org/develop/TeamProjectPanaderia/cliente/dto/ClienteResponseDto.java
+++ src/main/java/org/develop/TeamProjectPanaderia/cliente/dto/ClienteResponseDto.java
@@ -1,4 +1,23 @@
 package org.develop.TeamProjectPanaderia.cliente.dto;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Builder;
+import org.develop.TeamProjectPanaderia.categoria.models.Categoria;
+import org.hibernate.validator.constraints.Length;
+
+import java.time.LocalDateTime;
+
 public class ClienteResponseDto {
+
+    private Long id;
+    private String nombreCompleto;
+    private String correo;
+    private String dni;
+    private String telefono;
+    private String producto;
+    private LocalDateTime fechaCreacion;
+    private LocalDateTime fechaActualizacion;
+    private String categoria;
 }
