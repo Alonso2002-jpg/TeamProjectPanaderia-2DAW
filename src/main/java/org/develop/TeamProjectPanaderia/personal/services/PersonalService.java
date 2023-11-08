762475a23c9099156701d3a5c8ddee7223181a5b
kevinbermudes <39884727+kevinbermudes@users.noreply.github.com>
2023-11-08 18:55:30 +0100

    dto-mapper

--- src/main/java/org/develop/TeamProjectPanaderia/personal/services/PersonalService.java
+++ src/main/java/org/develop/TeamProjectPanaderia/personal/services/PersonalService.java
@@ -3,13 +3,14 @@ package org.develop.TeamProjectPanaderia.personal.services;
 import org.develop.TeamProjectPanaderia.personal.models.Personal;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface PersonalService {
     List<Personal>findAll();
-    Personal findById(Long id);
+    Personal findById(UUID id);
     Personal save(Personal personal);
-    Personal update(Long id,Personal personal);
-    void deleteById(Long id);
+    Personal update(UUID id,Personal personal);
+    void deleteById(UUID id);
     void deleteAll();
 
 }
--- src/main/java/org/develop/TeamProjectPanaderia/personal/services/PersonalServiceImpl.java
+++ src/main/java/org/develop/TeamProjectPanaderia/personal/services/PersonalServiceImpl.java
@@ -1,6 +1,7 @@
 package org.develop.TeamProjectPanaderia.personal.services;
 
 import lombok.extern.slf4j.Slf4j;
+import org.develop.TeamProjectPanaderia.personal.mapper.PersonalMapper;
 import org.develop.TeamProjectPanaderia.personal.repositories.PersonalRepository;
 import org.springframework.stereotype.Service;
 
@@ -8,11 +9,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class PersonalServiceImpl {
     private final PersonalRepository personalRepository;
+    private final PersonalMapper personalMapper;
 
-    public PersonalServiceImpl(PersonalRepository personalRepository) {
+
+    public PersonalServiceImpl(PersonalRepository personalRepository, PersonalMapper personalMapper) {
         this.personalRepository = personalRepository;
+        this.personalMapper = personalMapper;
     }
-    //  private final PersonalMapper personalMapper;
 
 
 
