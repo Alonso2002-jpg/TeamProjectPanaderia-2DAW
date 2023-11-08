c5b237265bbfc3113c3cf56ef4d2ba230404f372
Alonso2002-jpg <jorgealonso0876@gmail.com>
2023-11-08 23:26:36 +0100

    Implementacion de Excepciones

---
+++ src/main/java/org/develop/TeamProjectPanaderia/storage/exceptions/StorageExceptions.java
@@ -0,0 +1,7 @@
+package org.develop.TeamProjectPanaderia.storage.exceptions;
+
+public abstract class StorageExceptions extends RuntimeException{
+    public StorageExceptions(String message) {
+        super(message);
+    }
+}
