c47e3216b8ba4389176dc032902ceca24c1f55b1
kevinbermudes <39884727+kevinbermudes@users.noreply.github.com>
2023-11-10 16:20:22 +0100

    Se quitan los @Data y @AllContructorsargs de los dto

--- src/main/java/org/develop/TeamProjectPanaderia/personal/dto/PersonalCreateDto.java
+++ src/main/java/org/develop/TeamProjectPanaderia/personal/dto/PersonalCreateDto.java
@@ -1,13 +1,6 @@
 package org.develop.TeamProjectPanaderia.personal.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
 
-@Builder
-@AllArgsConstructor
-@Data
 public record PersonalCreateDto(
         String dni,
         String name,
