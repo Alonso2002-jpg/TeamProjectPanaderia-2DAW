aa0d162a677bf0343a13877c2ee19427064a931f
Miguel Zanotto <migzanotto18@hotmail.com>
2023-11-10 17:35:58 +0100

    Configuramos el findAll con sus criterias

--- src/main/java/org/develop/TeamProjectPanaderia/producto/repositories/ProductoRepository.java
+++ src/main/java/org/develop/TeamProjectPanaderia/producto/repositories/ProductoRepository.java
@@ -2,6 +2,8 @@ package org.develop.TeamProjectPanaderia.producto.repositories;
 
 import org.develop.TeamProjectPanaderia.producto.models.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -10,18 +12,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface ProductoRepository extends JpaRepository<Producto, UUID> {
-
-    @Query("SELECT p FROM Producto p WHERE LOWER(p.categoria.nameCategory) LIKE LOWER(:categoria)")
-    List<Producto> findAllByCategoriaContainsIgnoreCase(String categoria);
-    List<Producto> findAllByPrecio(Double precio);
-    List<Producto> findAllByNombreContainsIgnoreCase(String nombre);
-    List<Producto> findAllByIsActivoFalse();
-    List<Producto> findAllByIsActivoTrue();
-    @Query("SELECT p FROM Producto p WHERE LOWER(p.categoria.nameCategory) LIKE LOWER(:categoria) AND LOWER(p.proveedor.NIF) LIKE LOWER(:proveedor)")
-    List<Producto> findAllByCategoriaContainsIgnoreCaseAndProveedorContainsIgnoreCase(String categoria, String proveedor);
-    @Query("SELECT p FROM Producto p WHERE LOWER(p.proveedor.NIF) LIKE LOWER(:proveedor)")
-    List<Producto> findAllByProveedorContainsIgnoreCase(String proveedor);
+public interface ProductoRepository extends JpaRepository<Producto, UUID>, JpaSpecificationExecutor<Producto> {
     @Override
     Optional<Producto> findById(UUID uuid);
     Optional<Producto> findByNombreEqualsIgnoreCase(String nombre);
--- src/main/java/org/develop/TeamProjectPanaderia/producto/services/ProductoService.java
+++ src/main/java/org/develop/TeamProjectPanaderia/producto/services/ProductoService.java
@@ -3,17 +3,20 @@ package org.develop.TeamProjectPanaderia.producto.services;
 import org.develop.TeamProjectPanaderia.producto.dto.ProductoCreateDto;
 import org.develop.TeamProjectPanaderia.producto.dto.ProductoUpdateDto;
 import org.develop.TeamProjectPanaderia.producto.models.Producto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ProductoService {
-    List<Producto> findAll(String categoria, String proveedor);
+    Page<Producto> findAll(Optional<String> nombre, Optional <Integer> stockMin, Optional<Double> precioMax, Optional<Boolean> isActivo, Optional<String> categoria, Optional<String> proveedor, Pageable pageable);
     Producto findById(UUID id);
     Producto findByName(String name);
     Producto save(ProductoCreateDto productoCreateDto);
     Producto update(UUID id, ProductoUpdateDto productoUpdateDto);
     void deleteById(UUID id);
 }
-
 
