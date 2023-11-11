b0883a3b012e5f7cbc8294cb600bf632226d7a3e
Alonso2002-jpg <jorgealonso0876@gmail.com>
2023-11-11 01:53:03 +0100

    Implementando Notificacion Web Sockets - Categoria

--- src/main/java/org/develop/TeamProjectPanaderia/categoria/services/CategoriaServiceImpl.java
+++ src/main/java/org/develop/TeamProjectPanaderia/categoria/services/CategoriaServiceImpl.java
@@ -1,6 +1,12 @@
 package org.develop.TeamProjectPanaderia.categoria.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.weaver.ast.Not;
+import org.develop.TeamProjectPanaderia.WebSockets.dto.NotificacionResponseDto;
+import org.develop.TeamProjectPanaderia.WebSockets.mapper.NotificacionMapper;
+import org.develop.TeamProjectPanaderia.WebSockets.model.Notificacion;
 import org.develop.TeamProjectPanaderia.categoria.dto.CategoriaCreateDto;
 import org.develop.TeamProjectPanaderia.categoria.dto.CategoriaResponseDto;
 import org.develop.TeamProjectPanaderia.categoria.dto.CategoriaUpdateDto;
@@ -9,9 +15,13 @@ import org.develop.TeamProjectPanaderia.categoria.exceptions.CategoriaNotSaveExc
 import org.develop.TeamProjectPanaderia.categoria.mapper.CategoriaMapper;
 import org.develop.TeamProjectPanaderia.categoria.models.Categoria;
 import org.develop.TeamProjectPanaderia.categoria.repositories.CategoriaRepository;
+import org.develop.TeamProjectPanaderia.config.websockets.WebSocketConfig;
+import org.develop.TeamProjectPanaderia.config.websockets.WebSocketHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -19,11 +29,24 @@ import java.util.List;
 public class CategoriaServiceImpl implements CategoriaService{
     private final CategoriaRepository categoriaRepository;
     private final CategoriaMapper categoriaMapper;
+    private final WebSocketConfig webSocketConfig;
+    private WebSocketHandler webSocketHandler;
+    private final ObjectMapper objMapper;
+    private final NotificacionMapper notificacionMapper;
 
     @Autowired
-    public CategoriaServiceImpl(CategoriaRepository categoriaRepository, CategoriaMapper categoriaMapper) {
+    public CategoriaServiceImpl(CategoriaRepository categoriaRepository,
+                                CategoriaMapper categoriaMapper,
+                                WebSocketConfig webSocketConfig,
+                                WebSocketHandler webSocketHandler,
+                                ObjectMapper objMapper,
+                                NotificacionMapper notificacionMapper) {
         this.categoriaRepository = categoriaRepository;
         this.categoriaMapper = categoriaMapper;
+        this.webSocketConfig = webSocketConfig;
+        this.webSocketHandler = webSocketHandler;
+        this.objMapper = objMapper;
+        this.notificacionMapper = notificacionMapper;
     }
 
     @Override
@@ -69,5 +92,38 @@ public class CategoriaServiceImpl implements CategoriaService{
     @Override
     public void deleteAll() {
         categoriaRepository.deleteAll();
+    }
+
+    void onChange(Notificacion.Tipo tipo, Categoria data){
+        log.debug("Servicio de productos onChange con tipo: " + tipo + " y datos: " + data);
+
+        if (webSocketHandler == null){
+            log.warn("No se ha podido enviar la Notificacion, no se encontro servicio");
+            webSocketHandler = this.webSocketConfig.webSocketHandler();
+        }
+        try{
+            Notificacion<NotificacionResponseDto> notificacion = new Notificacion<>(
+                    "Categoria",
+                    tipo,
+                    notificacionMapper.getNotificacionResponseDto(data),
+                    LocalDate.now().toString()
+            );
+
+            String json = objMapper.writeValueAsString((notificacion));
+
+            log.info("Enviando Notificacion a los Clientes WS");
+
+            Thread senderThread = new Thread(()->{
+                try{
+                    webSocketHandler.sendMessage(json);
+                } catch (IOException e) {
+                    log.error("Error Enviando el Mensaje atraves del WS", e);
+                }
+            });
+
+            senderThread.start();
+        } catch (JsonProcessingException e) {
+             log.error("Error al convertir la notificación a JSON", e);
+        }
     }
 }
