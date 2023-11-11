636664db42f84e17257d9c5d61e11ae898f1df83
Laura Garrido Arredondo <anoobmedic@gmail.com>
2023-11-11 19:31:33 +0100

    Realizado test de ProveedorMapper

--- src/test/java/org/develop/TeamProjectPanaderia/proveedores/mapper/ProveedorMapperTest.java
+++ src/test/java/org/develop/TeamProjectPanaderia/proveedores/mapper/ProveedorMapperTest.java
@@ -1,5 +1,55 @@
 package org.develop.TeamProjectPanaderia.proveedores.mapper;
 
+import org.develop.TeamProjectPanaderia.proveedores.dto.ProveedorCreateDto;
+import org.develop.TeamProjectPanaderia.proveedores.dto.ProveedorResponseDto;
+import org.develop.TeamProjectPanaderia.proveedores.dto.ProveedorUpdateDto;
+import org.develop.TeamProjectPanaderia.proveedores.models.Proveedor;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ProveedorMapperTest {
 
+    private ProveedorMapper proveedorMapper;
+    private Proveedor proveedor;
+    private ProveedorCreateDto createDto;
+    private ProveedorUpdateDto updateDto;
+    private ProveedorResponseDto responseDto;
+
+    @BeforeEach
+    void setup() throws Exception {
+        proveedorMapper = new ProveedorMapper();
+        proveedor = Proveedor.builder()
+                .id(1L)
+                .numero("12")
+                .nombre("Juan")
+                .build();
+
+        createDto = proveedorMapper.ToCreate(proveedor);
+        updateDto = proveedorMapper.ToUpdate(proveedor);
+        responseDto = proveedorMapper.toResponse(proveedor);
+    }
+
+    @Test
+    void toCreate() {
+        assertEquals(proveedor.getId(), createDto.getId());
+        assertEquals(proveedor.getNumero(), createDto.getNumero());
+        assertEquals(proveedor.getNombre(), createDto.getNombre());
+    }
+
+    @Test
+    void toUpdate() {
+        assertEquals(proveedor.getId(), updateDto.getId());
+        assertEquals(proveedor.getNumero(), updateDto.getNumero());
+        assertEquals(proveedor.getNombre(), updateDto.getNombre());
+    }
+
+    @Test
+    void toResponse() {
+        assertEquals(proveedor.getId(), responseDto.getId());
+        assertEquals(proveedor.getNumero(), responseDto.getNumero());
+        assertEquals(proveedor.getNombre(), responseDto.getNombre());
+
+    }
 }
