cd5ec5b8170744fb73c64ea6194f62802ec5b6f5
Joselyn Obando <obandofernandez1@gmail.com>
2023-11-11 19:20:07 +0100

    Modificaciones en el service- ClienteServiceImpl

--- src/main/java/org/develop/TeamProjectPanaderia/cliente/dto/ClienteResponseDto.java
+++ src/main/java/org/develop/TeamProjectPanaderia/cliente/dto/ClienteResponseDto.java
@@ -1,24 +1,21 @@
 package org.develop.TeamProjectPanaderia.cliente.dto;
 
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
+
 import lombok.Builder;
-import org.develop.TeamProjectPanaderia.categoria.models.Categoria;
-import org.hibernate.validator.constraints.Length;
+
 
 import java.time.LocalDateTime;
 
 @Builder
 public class ClienteResponseDto {
 
-    private Long id;
-    private String nombreCompleto;
-    private String correo;
-    private String dni;
-    private String telefono;
-    private String producto;
-    private LocalDateTime fechaCreacion;
-    private LocalDateTime fechaActualizacion;
-    private String categoria;
+    Long id;
+    String nombreCompleto;
+    String correo;
+    String dni;
+    String telefono;
+    LocalDateTime fechaCreacion;
+    LocalDateTime fechaActualizacion;
+    String producto;
+    String categoria;
 }
