e43a12f742cc244bc7eac43a0c3c3d32a6ca3a5b
Alonso2002-jpg <jorgealonso0876@gmail.com>
2023-11-11 19:25:20 +0100

    Implementacion Test en Repo y Service

--- src/test/java/org/develop/TeamProjectPanaderia/categoria/services/CategoriaServiceImplTest.java
+++ src/test/java/org/develop/TeamProjectPanaderia/categoria/services/CategoriaServiceImplTest.java
@@ -1,10 +1,17 @@
 package org.develop.TeamProjectPanaderia.categoria.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.develop.TeamProjectPanaderia.WebSockets.mapper.NotificacionMapper;
+import org.develop.TeamProjectPanaderia.WebSockets.model.Notificacion;
 import org.develop.TeamProjectPanaderia.categoria.dto.CategoriaCreateDto;
+import org.develop.TeamProjectPanaderia.categoria.dto.CategoriaResponseDto;
 import org.develop.TeamProjectPanaderia.categoria.dto.CategoriaUpdateDto;
 import org.develop.TeamProjectPanaderia.categoria.mapper.CategoriaMapper;
 import org.develop.TeamProjectPanaderia.categoria.models.Categoria;
 import org.develop.TeamProjectPanaderia.categoria.repositories.CategoriaRepository;
+import org.develop.TeamProjectPanaderia.config.websockets.WebSocketConfig;
+import org.develop.TeamProjectPanaderia.config.websockets.WebSocketHandler;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,6 +19,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,15 +29,19 @@ import static org.mockito.Mockito.*;
 class CategoriaServiceImplTest {
     private Categoria categoria1, categoria;
 
+    private WebSocketHandler webSocketHandler = mock(WebSocketHandler.class);
+    private ObjectMapper objectMapper = mock(ObjectMapper.class);
     @Mock
     private CategoriaRepository categoriaRepository;
+    @Mock
+    private NotificacionMapper<CategoriaResponseDto> notificacionMapper;
     @Mock
     private CategoriaMapper categoriaMapper;
     @InjectMocks
     private CategoriaServiceImpl categoriaService;
 
     @BeforeEach
-    void setUp(){
+    void setUp() {
         categoria1 = Categoria.builder()
                 .id(1L)
                 .nameCategory("Chucherias")
@@ -41,9 +53,10 @@ class CategoriaServiceImplTest {
                 .isActive(false)
                 .build();
     }
+
     @Test
-    void findAll(){
-        List<Categoria> categorias = List.of(categoria,categoria1);
+    void findAll() {
+        List<Categoria> categorias = List.of(categoria, categoria1);
 
         when(categoriaRepository.findAll()).thenReturn(categorias);
         var result = categoriaService.findAll(null);
@@ -51,31 +64,31 @@ class CategoriaServiceImplTest {
         assertAll(
                 () -> assertNotNull(result),
                 () -> assertFalse(result.isEmpty()),
-                () -> assertEquals(2,result.size()),
-                ()-> assertEquals("Chucherias",result.get(1).getNameCategory()),
-                ()-> assertEquals("Bebidas",result.get(0).getNameCategory())
+                () -> assertEquals(2, result.size()),
+                () -> assertEquals("Chucherias", result.get(1).getNameCategory()),
+                () -> assertEquals("Bebidas", result.get(0).getNameCategory())
         );
 
-        verify(categoriaRepository,times(1)).findAll();
+        verify(categoriaRepository, times(1)).findAll();
     }
 
     @Test
-    void findById(){
+    void findById() {
         when(categoriaRepository.findById(1L)).thenReturn(java.util.Optional.of(categoria1));
         var result = categoriaService.findById(1L);
 
         assertAll(
                 () -> assertNotNull(result),
-                () -> assertEquals(1,result.getId()),
-                () -> assertEquals("Chucherias",result.getNameCategory())
+                () -> assertEquals(1, result.getId()),
+                () -> assertEquals("Chucherias", result.getNameCategory())
 
         );
 
-        verify(categoriaRepository,times(1)).findById(1L);
+        verify(categoriaRepository, times(1)).findById(1L);
     }
 
     @Test
-    void findByIdError(){
+    void findByIdError() {
         when(categoriaRepository.findById(100L)).thenReturn(java.util.Optional.empty());
         var result = assertThrows(Exception.class, () -> categoriaService.findById(100L));
 
@@ -83,64 +96,67 @@ class CategoriaServiceImplTest {
     }
 
     @Test
-    void findByName(){
+    void findByName() {
         when(categoriaRepository.findByNameCategoryIgnoreCase("Chucherias")).thenReturn(Optional.of(categoria1));
         var result = categoriaService.findByName("Chucherias");
 
         assertAll(
                 () -> assertNotNull(result),
-                () -> assertEquals(1,result.getId()),
-                () -> assertEquals("Chucherias",result.getNameCategory())
+                () -> assertEquals(1, result.getId()),
+                () -> assertEquals("Chucherias", result.getNameCategory())
         );
 
-        verify(categoriaRepository,times(1)).findByNameCategoryIgnoreCase("Chucherias");
+        verify(categoriaRepository, times(1)).findByNameCategoryIgnoreCase("Chucherias");
     }
 
     @Test
-    void findByNameError(){
+    void findByNameError() {
         when(categoriaRepository.findByNameCategoryIgnoreCase("Not Exists")).thenReturn(Optional.empty());
         var result = assertThrows(Exception.class, () -> categoriaService.findByName("Not Exists"));
 
         assertEquals("Categoria not found with name Not Exists", result.getMessage());
     }
+
     @Test
-    void findByIsActive(){
+    void findByIsActive() {
         when(categoriaRepository.findByIsActive(true)).thenReturn(List.of(categoria1));
         var result = categoriaService.findByActiveIs(true);
 
         assertAll(
                 () -> assertNotNull(result),
                 () -> assertFalse(result.isEmpty()),
-                () -> assertEquals(1,result.size()),
-                () -> assertEquals("Chucherias",result.get(0).getNameCategory())
+                () -> assertEquals(1, result.size()),
+                () -> assertEquals("Chucherias", result.get(0).getNameCategory())
         );
 
-        verify(categoriaRepository,times(1)).findByIsActive(true);
+        verify(categoriaRepository, times(1)).findByIsActive(true);
     }
+
     @Test
-    void save(){
-        CategoriaCreateDto createDto = new CategoriaCreateDto(categoria1.getNameCategory(),categoria1.isActive());
+    void save() throws IOException {
+        CategoriaCreateDto createDto = new CategoriaCreateDto(categoria1.getNameCategory(), categoria1.isActive());
         when(categoriaMapper.toCategoria(createDto)).thenReturn(categoria1);
         when(categoriaRepository.findByNameCategoryIgnoreCase(categoria1.getNameCategory())).thenReturn(java.util.Optional.empty());
         when(categoriaRepository.save(categoria1)).thenReturn(categoria1);
-
+        when(objectMapper.writeValueAsString(anyString())).thenReturn(categoria1.toString());
+        doNothing().when(webSocketHandler).sendMessage(any());
         var result = categoriaService.save(createDto);
 
         assertAll(
                 () -> assertNotNull(result),
-                () -> assertEquals(1,result.getId()),
-                () -> assertEquals("Chucherias",result.getNameCategory()),
+                () -> assertEquals(1, result.getId()),
+                () -> assertEquals("Chucherias", result.getNameCategory()),
                 () -> assertTrue(result.isActive())
         );
 
-        verify(categoriaRepository,times(1)).findByNameCategoryIgnoreCase(categoria1.getNameCategory());
-        verify(categoriaRepository,times(1)).save(categoria1);
-        verify(categoriaMapper,times(1)).toCategoria(createDto);
+        verify(categoriaRepository, times(1)).findByNameCategoryIgnoreCase(categoria1.getNameCategory());
+        verify(categoriaRepository, times(1)).save(categoria1);
+        verify(categoriaMapper, times(1)).toCategoria(createDto);
     }
 
     @Test
-    void saveError(){
-        CategoriaCreateDto createDto = new CategoriaCreateDto("Not Exists",categoria1.isActive());
+    void saveError() {
+        CategoriaCreateDto createDto = new CategoriaCreateDto("Not Exists", categoria1.isActive());
 
         when(categoriaRepository.findByNameCategoryIgnoreCase("Not Exists")).thenReturn(java.util.Optional.of(categoria1));
 
@@ -150,45 +166,55 @@ class CategoriaServiceImplTest {
                 () -> assertEquals("Category already exists", result.getMessage())
         );
 
-        verify(categoriaRepository,times(1)).findByNameCategoryIgnoreCase("Not Exists");
+        verify(categoriaRepository, times(1)).findByNameCategoryIgnoreCase("Not Exists");
     }
 
     @Test
-    void update(){
-        CategoriaUpdateDto updDto = new CategoriaUpdateDto(categoria1.getNameCategory(),categoria1.isActive());
+    void update() throws IOException {
+        CategoriaUpdateDto updDto = new CategoriaUpdateDto(categoria1.getNameCategory(), categoria1.isActive());
 
-        when(categoriaMapper.toCategoria(updDto,categoria1)).thenReturn(categoria1);
+        when(categoriaMapper.toCategoria(updDto, categoria1)).thenReturn(categoria1);
         when(categoriaRepository.findById(1L)).thenReturn(java.util.Optional.of(categoria1));
         when(categoriaRepository.save(categoria1)).thenReturn(categoria1);
+        when(objectMapper.writeValueAsString(anyString())).thenReturn(categoria1.toString());
+        doNothing().when(webSocketHandler).sendMessage(any());
 
-        var result = categoriaService.update(1L,updDto);
+        var result = categoriaService.update(1L, updDto);
 
         assertAll(
                 () -> assertNotNull(result),
-                () -> assertEquals(1,result.getId()),
-                () -> assertEquals(categoria1.getNameCategory(),result.getNameCategory()),
+                () -> assertEquals(1, result.getId()),
+                () -> assertEquals(categoria1.getNameCategory(), result.getNameCategory()),
                 () -> assertTrue(result.isActive())
         );
 
-        verify(categoriaRepository,times(1)).findById(1L);
-        verify(categoriaRepository,times(1)).save(categoria1);
-        verify(categoriaMapper,times(1)).toCategoria(updDto,categoria1);
+        verify(categoriaRepository, times(1)).findById(1L);
+        verify(categoriaRepository, times(1)).save(categoria1);
+        verify(categoriaMapper, times(1)).toCategoria(updDto, categoria1);
     }
 
     @Test
-    void deleteById(){
+    void deleteById() throws IOException {
         when(categoriaRepository.findById(1L)).thenReturn(java.util.Optional.of(categoria1));
         doNothing().when(categoriaRepository).deleteById(1L);
         categoriaService.deleteById(1L);
+        when(objectMapper.writeValueAsString(anyString())).thenReturn(categoria1.toString());
+        doNothing().when(webSocketHandler).sendMessage(any());
 
-        verify(categoriaRepository,times(1)).findById(1L);
+        verify(categoriaRepository, times(1)).findById(1L);
     }
 
     @Test
-    void deleteAll(){
+    void deleteAll() {
         doNothing().when(categoriaRepository).deleteAll();
         categoriaService.deleteAll();
 
-        verify(categoriaRepository,times(1)).deleteAll();
+        verify(categoriaRepository, times(1)).deleteAll();
+    }
+
+    @Test
+    void onChange() throws IOException {
+        doNothing().when(webSocketHandler).sendMessage(any());
+        categoriaService.onChange(Notificacion.Tipo.CREATE, categoria1);
     }
 }
