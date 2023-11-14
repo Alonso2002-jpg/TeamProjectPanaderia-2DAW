1395b119044299f64138f42f1b345f9f2f9616f0
kevinbermudes <39884727+kevinbermudes@users.noreply.github.com>
2023-11-14 19:43:09 +0100

    correccion de ahora se llama personalNotSaved

--- src/main/java/org/develop/TeamProjectPanaderia/personal/exceptions/PersonalNotSaved.java
+++ src/main/java/org/develop/TeamProjectPanaderia/personal/exceptions/PersonalNotSaved.java
@@ -1,12 +1,11 @@
 package org.develop.TeamProjectPanaderia.personal.exceptions;
 
-import lombok.Builder;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
-public class PerosnalNotsaveEcception extends RuntimeException {
-    public PerosnalNotsaveEcception(String dni) {
+public class PersonalNotSaved extends RuntimeException {
+    public PersonalNotSaved(String dni) {
         super("El dni " + dni + " ya esta registrado en la BD ");
     }
 }
