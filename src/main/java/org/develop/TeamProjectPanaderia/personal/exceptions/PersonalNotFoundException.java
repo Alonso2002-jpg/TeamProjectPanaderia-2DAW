4fc408c0773c4223452ef8df7eb12fdfea184169
kevinbermudes <39884727+kevinbermudes@users.noreply.github.com>
2023-11-15 19:58:29 +0100

    Correccion PersonalNotFount

--- src/main/java/org/develop/TeamProjectPanaderia/personal/exceptions/PersonalNotFoundException.java
+++ src/main/java/org/develop/TeamProjectPanaderia/personal/exceptions/PersonalNotFoundException.java
@@ -3,9 +3,15 @@ package org.develop.TeamProjectPanaderia.personal.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.util.UUID;
+
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public abstract class PersonalNotFoundException extends RuntimeException{
-    public PersonalNotFoundException(String message) {
-        super(message);
+public class PersonalNotFoundException extends RuntimeException{
+    public PersonalNotFoundException(String dni) {
+        super("Personal con dni " + dni + " no encontrado");
+    }
+
+    public PersonalNotFoundException(UUID uuid) {
+        super("Personal con id " + uuid + " no encontrado");
     }
 }
