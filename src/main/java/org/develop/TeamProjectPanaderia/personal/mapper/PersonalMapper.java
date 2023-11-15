5038548402397995d3fc4b31695a1d9511956560
kevinbermudes <39884727+kevinbermudes@users.noreply.github.com>
2023-11-15 19:59:07 +0100

    Correccion PersonalMapper

--- src/main/java/org/develop/TeamProjectPanaderia/personal/mapper/PersonalMapper.java
+++ src/main/java/org/develop/TeamProjectPanaderia/personal/mapper/PersonalMapper.java
@@ -5,6 +5,7 @@ import org.develop.TeamProjectPanaderia.personal.dto.PersonalResponseDto;
 import org.develop.TeamProjectPanaderia.personal.dto.PersonalCreateDto;
 import org.develop.TeamProjectPanaderia.personal.dto.PersonalUpdateDto;
 import org.develop.TeamProjectPanaderia.personal.models.Personal;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
@@ -14,54 +15,52 @@ import java.util.UUID;
 
 @Component
 public class PersonalMapper {
-    public Personal toPersonal(UUID id, Categoria categoria, PersonalCreateDto dto) {
+    public Personal toPersonalCreate(UUID id, Categoria categoria, PersonalCreateDto dto) {
         return Personal.builder()
+                .id(id)
                 .dni(dto.dni())
-                .isActive(dto.isActive())
-                .build();
-    }
-
-    public PersonalResponseDto toResponse(Personal personal) {
-        return  new PersonalResponseDto(
-                personal.getDni(),
-                personal.getNombre(),
-                personal.getSeccion().getNameCategory(),
-                personal.getFechaAlta().toString(),
-                personal.getFechaBaja().toString(),
-                personal.getFechaCreacion().toString(),
-                personal.getFechaUpdate().toString(),
-                personal.isActive()
-
-        );
-    }
-    public  Personal toPersonalCreate(PersonalCreateDto personalDto, Categoria categoria){
-        return Personal.builder()
-                .dni(personalDto.dni())
-                .nombre(personalDto.name())
+                .nombre(dto.nombre())
                 .seccion(categoria)
-                .isActive(personalDto.isActive())
+                .isActive(dto.isActive() != null ? dto.isActive() : true)
+                .fechaAlta(LocalDate.now())
+                .fechaBaja(null)
+                .fechaCreacion(LocalDateTime.now())
+                .fechaActualizacion(LocalDateTime.now())
                 .build();
     }
 
-
-
-
-
-    public List<PersonalResponseDto> toResponseList(List<Personal> personals){
-        return personals.stream()
-                .map(this::toResponse)
-                .toList();
-    }
-
-    public Personal toPersonalUdate(PersonalUpdateDto personalDto, Personal personalUpd, Categoria categoria) {
+    public Personal toPersonalUpdate(PersonalUpdateDto personalDto, Personal personalUpd, Categoria categoria) {
         return Personal.builder()
+                .id(personalUpd.getId())
                 .dni(personalUpd.getDni())
                 .nombre(personalDto.nombre() == null ? personalUpd.getNombre() : personalDto.nombre())
-                .seccion(categoria == null ? personalUpd.getSeccion() : categoria)
+                .seccion(categoria)
                 .fechaAlta(personalUpd.getFechaAlta())
-                .fechaBaja(personalDto.fechaBaja() == null ? personalUpd.getFechaBaja() : personalDto.fechaBaja())
+                .fechaBaja(personalUpd.getFechaBaja())
+                .fechaCreacion(personalUpd.getFechaCreacion())
+                .fechaActualizacion(LocalDateTime.now())
                 .isActive(personalDto.isActive() == null ? personalUpd.isActive() : personalDto.isActive())
                 .build();
     }
 
+    public PersonalResponseDto toResponseDto(Personal personal) {
+        return  new PersonalResponseDto(
+                personal.getId(),
+                personal.getDni(),
+                personal.getNombre(),
+                personal.getSeccion().getNameCategory(),
+                personal.getFechaAlta().toString(),
+                personal.isActive()
+        );
+    }
+
+    public List<PersonalResponseDto> toResponseList(List<Personal> personals){
+        return personals.stream()
+                .map(this::toResponseDto)
+                .toList();
+    }
+
+    public Page<PersonalResponseDto> toPageResponse(Page<Personal> personalPages){
+        return personalPages.map(this::toResponseDto);
+    }
 }
