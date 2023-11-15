1bcb6ef0ff046d7f1e79a8c4695c6b0aa3d70c61
Alonso2002-jpg <jorgealonso0876@gmail.com>
2023-11-15 19:52:32 +0100

    Implementacion CommandLineRuner - EnableCaching

--- src/main/java/org/develop/TeamProjectPanaderia/TeamProjectPanaderiaApplication.java
+++ src/main/java/org/develop/TeamProjectPanaderia/TeamProjectPanaderiaApplication.java
@@ -1,13 +1,21 @@
 package org.develop.TeamProjectPanaderia;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-public class TeamProjectPanaderiaApplication {
+@EnableCaching
+public class TeamProjectPanaderiaApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TeamProjectPanaderiaApplication.class, args);
 	}
 
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Servidor Corriendo en puerto 8080 🦖​🕊️​");
+	}
 }
