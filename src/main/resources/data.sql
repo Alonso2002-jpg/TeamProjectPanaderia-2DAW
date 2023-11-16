ac8b55d121c73a6d08af841c67a25714ffbd6a44
Alonso2002-jpg <jorgealonso0876@gmail.com>
2023-11-16 02:09:38 +0100

    Implementacion de inserts SQL

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,94 +1,72 @@
 -- Inserts Categorias
-INSERT INTO Categorias (nameCategory, isActive)
-VALUES ('Dulces', true);
+INSERT INTO CATEGORIAS (name, active) VALUES ('Dulces', TRUE);--1
+INSERT INTO CATEGORIAS (name, active) VALUES ('Bebidas', TRUE);--2
+INSERT INTO CATEGORIAS (name, active) VALUES ('Utensilios', TRUE);--3
+INSERT INTO CATEGORIAS (name, active) VALUES ('Repostería', TRUE);--4
+INSERT INTO CATEGORIAS (name, active) VALUES ('Atención al cliente', TRUE);--5
+INSERT INTO CATEGORIAS (name, active) VALUES ('Limpieza', TRUE);--6
+INSERT INTO CATEGORIAS (name, active) VALUES ('Reparto', TRUE);--7
+INSERT INTO CATEGORIAS (name, active) VALUES ('VIP', TRUE);--8
+INSERT INTO CATEGORIAS (name, active) VALUES ('Normal', TRUE);--9
+INSERT INTO CATEGORIAS (name, active) VALUES ('Distribuidor', TRUE);--10
+INSERT INTO CATEGORIAS (name, active) VALUES ('Mayorista', TRUE);--11
+INSERT INTO CATEGORIAS (name, active) VALUES ('Proveedor', TRUE);--12
 
-INSERT INTO Categorias (nameCategory, isActive)
-VALUES ('Bebidas', true);
-
-INSERT INTO Categorias (nameCategory, isActive)
-VALUES ('Utensilios', true);
-
-INSERT INTO Categorias (nameCategory, isActive)
-VALUES ('Repostería', true);
-
-INSERT INTO Categorias (nameCategory, isActive)
-VALUES ('Atención al cliente', true);
-
-INSERT INTO Categorias (nameCategory, isActive)
-VALUES ('Limpieza', true);
-
-INSERT INTO Categorias (nameCategory, isActive)
-VALUES ('Reparto', true);
-
-INSERT INTO Categorias (nameCategory, isActive)
-VALUES ('VIP', true);
-
-INSERT INTO Categorias (nameCategory, isActive)
-VALUES ('Normal', true);
-
-INSERT INTO Categorias (nameCategory, isActive)
-VALUES ('Distribuidor', true);
-
-INSERT INTO Categorias (nameCategory, isActive)
-VALUES ('Mayorista', true);
-
-INSERT INTO Categorias (nameCategory, isActive)
-VALUES ('Proveedor', true);
 
 --INSERTS PROVEEDORES
-INSERT INTO Proveedores (nif, tipo, numero, nombre)
-VALUES ('12345678A', 'Distribuidor', '123456789', 'Distribuciones Dulces');
+INSERT INTO Proveedores (nif, tipo, numero, name)--1
+VALUES ('12345678A', 10, '123456789', 'Distribuciones Dulces');
 
-INSERT INTO Proveedores (nif, tipo, numero, nombre)
-VALUES ('98765432B', 'Mayorista', '987654321', 'Harina y Más');
+INSERT INTO Proveedores (nif, tipo, numero, name)--2
+VALUES ('98765432B', 11, '987654321', 'Harina y Más');
 
-INSERT INTO Proveedores (nif, tipo, numero, nombre)
-VALUES ('87654321C', 'Proveedor', '123987456', 'Proveedores Bebidas');
+INSERT INTO Proveedores (nif, tipo, numero, name)--3
+VALUES ('87654321C', 12, '123987456', 'Proveedores Bebidas');
 
-INSERT INTO Proveedores (nif, tipo, numero, nombre)
-VALUES ('34567891D', 'Distribuidor', '654321789', 'Proveedores Utensilios');
+INSERT INTO Proveedores (nif, tipo, numero, name)--4
+VALUES ('34567891D', 10, '654321789', 'Proveedores Utensilios');
 
 --INSERTS PRODUCTOS
-INSERT INTO Productos (nombre, stock, precio, is_activo, categoria, proveedor)
-VALUES ('Pan de Leche', 100, 2.5, true, 'Dulces', '12345678A');
+INSERT INTO Productos (id,nombre, stock, precio, is_activo, categoria_id, proveedor_id)
+VALUES (UUID(),'Pan de Leche', 100, 2.5, true, 1, 1);
 
-INSERT INTO Productos (nombre, stock, precio, is_activo, categoria, proveedor)
-VALUES ('Galletas de Chocolate', 150, 3.0, true, 'Dulces', '98765432B');
+INSERT INTO Productos (id,nombre, stock, precio, is_activo, categoria_id, proveedor_id)
+VALUES (UUID(),'Galletas de Chocolate', 150, 3.0, true, 1, 2);
 
-INSERT INTO Productos (nombre, stock, precio, is_activo, categoria, proveedor)
-VALUES ('Agua Mineral', 200, 1.0, true, 'Bebidas', '87654321C');
+INSERT INTO Productos (id,nombre, stock, precio, is_activo, categoria_id, proveedor_id)
+VALUES (UUID(),'Agua Mineral', 200, 1.0, true, 2, 3);
 
-INSERT INTO Productos (nombre, stock, precio, is_activo, categoria, proveedor)
-VALUES ('Taza de Café', 50, 5.0, true, 'Utensilios', '34567891D');
+INSERT INTO Productos (id,nombre, stock, precio, is_activo, categoria_id, proveedor_id)
+VALUES (UUID(),'Taza de Café', 50, 5.0, true, 3, 4);
 
 --INSERTS PERSONAL
-INSERT INTO Personal (dni, nombre, seccion, is_active)
-VALUES ('12345678A', 'Juan Pérez', 'Cocina', true);
+INSERT INTO Personal (id,dni, nombre, seccion, active)
+VALUES (UUID(),'12345678A', 'Juan Pérez', 4, true);
 
-INSERT INTO Personal (dni, nombre, seccion, is_active)
-VALUES ('23456789B', 'María López', 'Repostería', true);
+INSERT INTO Personal (id,dni, nombre, seccion, active)
+VALUES (UUID(),'23456789B', 'María López', 4, true);
 
-INSERT INTO Personal (dni, nombre, seccion, is_active)
-VALUES ('34567891C', 'Pedro Rodríguez', 'Atención al cliente', true);
+INSERT INTO Personal (id, dni, nombre, seccion, active)
+VALUES (UUID(),'34567891C', 'Pedro Rodríguez', 5, true);
 
-INSERT INTO Personal (dni, nombre, seccion, is_active)
-VALUES ('45678912D', 'Laura García', 'Limpieza', true);
+INSERT INTO Personal (id, dni, nombre, seccion, active)
+VALUES (UUID(),'45678912D', 'Laura García', 6, true);
 
-INSERT INTO Personal (dni, nombre, seccion, is_active)
-VALUES ('56789123E', 'Carlos Martínez', 'Reparto', true);
+INSERT INTO Personal (id, dni, nombre, seccion, active)
+VALUES (UUID(),'56789123E', 'Carlos Martínez', 7, true);
 
 --INSERTS CLIENTES
-INSERT INTO Cliente (nombre_completo, correo, dni, telefono, imagen, categoria, is_active)
-VALUES ('Laura González', 'laura@example.com', '12345678A', '612345678', 'imagen1.jpg', 'Normal', true);
+INSERT INTO CLIENTES (nombre_completo, correo, dni, telefono, imagen, categoria_id, is_active)
+VALUES ('Laura González', 'laura@example.com', '12345678A', '612345678', 'imagen1.jpg', 9, true);
 
-INSERT INTO Cliente (nombre_completo, correo, dni, telefono, imagen, categoria, is_active)
-VALUES ('Pablo Pérez', 'pablo@example.com', '23456789B', '655432167', 'imagen2.jpg', 'Normal', true);
+INSERT INTO CLIENTES (nombre_completo, correo, dni, telefono, imagen, categoria_id, is_active)
+VALUES ('Pablo Pérez', 'pablo@example.com', '23456789B', '655432167', 'imagen2.jpg', 9, true);
 
-INSERT INTO Cliente (nombre_completo, correo, dni, telefono, imagen, categoria, is_active)
-VALUES ('María Rodríguez', 'maria@example.com', '34567891C', '699876543', 'imagen3.jpg', 'VIP', true);
+INSERT INTO CLIENTES (nombre_completo, correo, dni, telefono, imagen, categoria_id, is_active)
+VALUES ('María Rodríguez', 'maria@example.com', '34567891C', '699876543', 'imagen3.jpg', 8, true);
 
-INSERT INTO Cliente (nombre_completo, correo, dni, telefono, imagen, categoria, is_active)
-VALUES ('Carlos Martínez', 'carlos@example.com', '45678912D', '666123456', 'imagen4.jpg', 'Normal', true);
+INSERT INTO CLIENTES (nombre_completo, correo, dni, telefono, imagen, categoria_id, is_active)
+VALUES ('Carlos Martínez', 'carlos@example.com', '45678912D', '666123456', 'imagen4.jpg', 9, true);
 
-INSERT INTO Cliente (nombre_completo, correo, dni, telefono, imagen, categoria, is_active)
-VALUES ('Lucía Sánchez', 'lucia@example.com', '56789123E', '677654321', 'imagen5.jpg', 'VIP', true);
+INSERT INTO CLIENTES (nombre_completo, correo, dni, telefono, imagen, categoria_id, is_active)
+VALUES ('Lucía Sánchez', 'lucia@example.com', '56789123E', '677654321', 'imagen5.jpg', 8, true);
