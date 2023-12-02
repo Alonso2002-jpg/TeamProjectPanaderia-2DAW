5672f0a7946e60a86072dbe0804e551cd45dcb10
Miguel Zanotto <migzanotto18@hotmail.com>
2023-12-02 22:21:44 +0100

    Configurado parcialmente el Swagger

--- src/main/java/org/develop/TeamProjectPanaderia/config/cors/CorsConfig.java
+++ src/main/java/org/develop/TeamProjectPanaderia/config/cors/CorsConfig.java
@@ -14,12 +14,28 @@ public class CorsConfig {
 
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/rest/**/**")
+                registry.addMapping("/rest/pedidos/**")
                         //.allowedOrigins("http://localhost:8080")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .maxAge(3600);
-            }
+                registry.addMapping("/rest/productos/**")
+                        //.allowedOrigins("http://localhost:8080")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .maxAge(3600);
+                registry.addMapping("/rest/categorias/**")
+                        //.allowedOrigins("http://localhost:8080")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .maxAge(3600);
+                registry.addMapping("/rest/clientes/**")
+                        //.allowedOrigins("http://localhost:8080")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .maxAge(3600);
+                registry.addMapping("/rest/proveedores/**")
+                        //.allowedOrigins("http://localhost:8080")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .maxAge(3600);
 
+            }
         };
     }
 }
---
+++ src/main/java/org/develop/TeamProjectPanaderia/config/swagger/SwaggerConfig.java
@@ -0,0 +1,75 @@
+package org.develop.TeamProjectPanaderia.config.swagger;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class SwaggerConfig {
+
+    @Value("${api.version}")
+    private String apiVersion;
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
+
+    @Bean
+    OpenAPI apiInfo() {
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("Panaderia Pepitos Spring")
+                                .version("1.0.0")
+                                .description("Somos los que te calientan el bollo")
+                                .termsOfService("https://github.com/Alonso2002-jpg/TeamProjectPanaderia-2DAW")
+                                .license(
+                                        new License()
+                                                .name("CC BY-NC-SA 4.0")
+                                                .url("https://github.com/Alonso2002-jpg/TeamProjectPanaderia-2DAW")
+                                )
+                                .contact(
+                                        new Contact()
+                                                .name("Miguel Angel Zanotot Rojas")
+                                                .email("miguelangel.zanotto@alumno.iesluisvives.org")
+                                                .url("https://github.com/MiguelZanottto")
+                                )
+
+                )
+                .externalDocs(
+                        new ExternalDocumentation()
+                                .description("Documentación del Proyecto")
+                                .url("https://github.com/Alonso2002-jpg/TeamProjectPanaderia-2DAW/blob/main/README.md")
+                )
+                .externalDocs(
+                        new ExternalDocumentation()
+                                .description("GitHub del Proyecto")
+                                .url("https://github.com/Alonso2002-jpg/TeamProjectPanaderia-2DAW")
+                )
+                .addSecurityItem(new SecurityRequirement().
+                        addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes
+                        ("Bearer Authentication", createAPIKeyScheme()));
+    }
+
+
+    @Bean
+    GroupedOpenApi httpApi() {
+        return GroupedOpenApi.builder()
+                .group("https")
+                .pathsToMatch("/" + apiVersion + "/productos/**")
+                .displayName("Panaderia Pepitos Swagger")
+                .build();
+    }
+}
